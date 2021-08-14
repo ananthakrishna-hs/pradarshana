39acb1d976370dbd64370e320ee6b8e7bededfba
Ananthakrishna H S <h.s.ananthakrishna@gmail.com>
2021-08-14 16:46:01 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "pradarshana",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
